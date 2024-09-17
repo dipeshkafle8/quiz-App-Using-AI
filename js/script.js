ea17bad8051b1687bd35b0ccf82603bd86230501
Dipesh <dipeshkaflen2060@gmail.com>
2024-09-17 10:03:25 +0530

    Intial commit

--- js/script.js
+++ js/script.js
@@ -8,3 +8,26 @@ const options = document.getElementById("options");
 const submit = document.getElementById("submit");
 const previous = document.getElementById("previous");
 const next = document.getElementById("next");
+
+let Questions;
+
+function displayQuestionsOnUI() {
+  Questions.forEach((element) => {
+    console.log(element);
+  });
+  console.log(typeof Questions);
+}
+
+async function getQuestionsFromAI() {
+  let result = await model.generateContent(
+    "Provide the random array of objects having properties like question option and correct answer(note:provide atleast 10 data,noting extra text only json so that i can do parse)"
+  );
+  let temp = result.response.text();
+  const tempQuestions = temp.split("\n");
+  tempQuestions.pop();
+  tempQuestions.shift();
+  Questions = JSON.parse(tempQuestions.join("\n"));
+  displayQuestionsOnUI();
+}
+
+getQuestionsFromAI();
